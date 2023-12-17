5ce50f9b7a003c7bbeb35bbcc4e3df5a8fb08887
Andrii Pervashov <51098401+videoman496@users.noreply.github.com>
2023-12-17 01:20:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,30 +1,51 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 #include "compressor.c"
 int result;
 int counts[128];
+void splashScreen() {
+    printf("**********************************************\n");
+    printf("*        RLE and Huffman Compression Tool    *\n");
+    printf("*               Andrii Pervashov             *\n");
+    printf("*              Predtermin Edition            *\n");
+    printf("**********************************************\n");
 
+    printf("\n");
+
+    printf("Please wait while the program initializes...  \n");
+
+    char spinChars[] = "|/-\\";
+    int spinIndex = 0;
+
+    // Остановить анимацию через 3 секунды
+    sleep(3);
+
+    // Очистить строку анимации и вернуться к началу
+    printf("\b\b\b\b\b\b"); // Убираем символы анимации и возвращаем курсор на начало
+    fflush(stdout); // Принудительно сбрасываем буфер вывода
+    printf("Initialization complete.\n\n");
+}
 int main(int argc, char* argv[]) {
-    
+    splashScreen();
 
     char *algorithm = argv[1];
     char *input_file_name = argv[2];
-    char *output_file_name = argv[3]; 
+    char *output_file_name = argv[3]; // Установим имя выходного файла по умолчанию
 
     if (strcmp(algorithm, "-h") == 0) {
         long file_size;
-        char *str = fileToString(argv[2], &file_size); 
+        char *str = fileToString(argv[2], &file_size); // Правильно передайте имя файла
         if (!str) {
             printf("Failed to read input file.\n");
             return 1;
         }
 
-        //  Calculating character probabilities
+        // Step 2: Calculate character probabilities
         calculate_prob(str, counts);
 
-        // Building Huffman Tree
+        // Step 3: Build Huffman Tree
         int node_count = 0;
         for (int i = 0; i < MAX_SZ; i++) {
             if (counts[i] != 0) node_count++;
@@ -47,11 +68,11 @@ int main(int argc, char* argv[]) {
         }
         Node *root = &nodes[0];
 
-        // Generating Huffman codes
+        // Step 4: Generate Huffman codes
         char path[MAX_CODE_LEN] = {0};
         dfs(root, path, 0);
 
-        //  Writing compressed file
+        // Step 5: Write compressed file
         FILE *compressedFile = fopen(argv[3], "w");
         if (!compressedFile) {
             printf("Failed to open compressed file for writing.\n");
@@ -65,10 +86,10 @@ int main(int argc, char* argv[]) {
                 writeBitToFile(code[i] - '0', compressedFile, 0);
             }
         }
-        writeBitToFile(0, compressedFile, 1); 
+        writeBitToFile(0, compressedFile, 1); // Flush remaining bits
         fclose(compressedFile);
 
-        // Reading and decompressing the file
+        // Step 6: Read and decompress the file
         compressedFile = fopen(argv[3], "rb");
         if (!compressedFile) {
             printf("Failed to open compressed file for reading.\n");
@@ -82,7 +103,8 @@ int main(int argc, char* argv[]) {
             return 1;
         }
 
-        decompress(compressedFile, root, decompressedFile, file_size); 
+        decompress(compressedFile, root, decompressedFile, file_size); // Pass original file size
+
         fclose(compressedFile);
         fclose(decompressedFile);
         free(str);
@@ -97,9 +119,11 @@ int main(int argc, char* argv[]) {
     if (strcmp(algorithm, "-r") == 0) {
         if(strcmp(argv[2],"-c")==0){
             compressUsingRLE(argv[3], "RLECompressed.txt");
+            printf("Compression using RLE completed successfully\n");
         }
         else if (strcmp(argv[2],"-d")==0){
             decompressUsingRLE("RLECompressed.txt", "RLEDecompressed.txt");
+            printf("Decompression using RLE completed successfully\n");
         }
 
 
@@ -107,7 +131,7 @@ int main(int argc, char* argv[]) {
             fprintf(stderr, "Compression using RLE failed\n");
             return 1;
         }
-        printf("Compression using RLE completed successfully\n");
+
     } else {
         fprintf(stderr, "Invalid algorithm. Use -h for Huffman or -r for RLE.\n");
         return 1;
